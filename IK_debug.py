58d01cb6575b479fe4bc89e6f74fde4243940c40
Antti Hietanen <antti.hietanen@tut.fi>
2017-09-16 18:45:40 +0300

    FK template

--- IK_debug.py
+++ IK_debug.py
@@ -2,6 +2,7 @@ from sympy import *
 from time import time
 from mpmath import radians
 import tf
+import numpy as np
 
 '''
 Format of test case is [ [[EE position],[EE orientation as quaternions]],[WC location],[joint angles]]
@@ -63,13 +64,106 @@ def test_code(test_case):
     ## 
 
     ## Insert IK code here!
-    
+    '''
+    theta1 = test_case[2][0]
+    theta2 = test_case[2][1]
+    theta3 = test_case[2][2]
+    theta4 = test_case[2][3]
+    theta5 = test_case[2][4]
+    theta6 = test_case[2][5]
+    '''
     theta1 = 0
     theta2 = 0
     theta3 = 0
     theta4 = 0
     theta5 = 0
     theta6 = 0
+
+
+    ### Create symbols for joint variables
+    # alpha: twist angle
+    # a: link length
+    # d: link offsets
+    # q: theta variables (note: not the joint angle!)
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+    ### KUKA KR210 ###
+    # DH Parameters
+    DH_table = {alpha0:     0,  a0:      0, d1:  0.75,  q1: q1,
+                alpha1: -pi/2,  a1:   0.35, d2:     0,  q2: q2-pi/2.,
+                alpha2:     0,  a2:   1.25, d3:     0,  q3: q3,
+                alpha3: -pi/2,  a3: -0.054, d4:  1.50,  q4: q4,
+                alpha4:  pi/2,  a4:      0, d5:     0,  q5: q5,
+                alpha5: -pi/2,  a5:      0, d6:     0,  q6: q7,
+                alpha6:     0,  a6:      0, d7: 0.303,  q7: 0}
+
+    def TF_Matrix(alpha, a, d, q):
+        TF = Matrix([[             cos(q),              -sin(q),            0,               a],
+                     [sin(q) * cos(alpha),  cos(q) * cos(alpha),  -sin(alpha), -sin(alpha) * d],
+                     [sin(q) * sin(alpha),  cos(q) * sin(alpha),   cos(alpha),  cos(alpha) * d],
+                     [                  0,                    0,            0,               1]])
+        return TF
+
+
+
+    ### Homogeneous Transforms between i and i-1 links
+    T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_table)
+    T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_table)
+    T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_table)
+    T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(DH_table)
+    T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(DH_table)
+    T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_table)
+    T6_G = TF_Matrix(alpha6, a6, d7, q7).subs(DH_table)
+
+    T0_2 = T0_1 * T1_2  # tf base_link to link2
+    T0_3 = T0_2 * T2_3  # tf base_link to link3
+    T0_4 = T0_3 * T3_4  # tf base_link to link4
+    T0_5 = T0_4 * T4_5  # tf base_link to link5
+    T0_6 = T0_5 * T5_6  # tf base_link to link6
+    T0_G = T0_6 * T6_G  # tf base_link to link7
+
+
+
+    R_z = Matrix([[    cos(pi),     -sin(pi),   0,  0],
+                  [    sin(pi),      cos(pi),   0,  0],
+                  [          0,            0,   1,  0],
+                  [          0,            0,   0,  1]])
+
+    R_y = Matrix([[ cos(-pi/2),     0,  sin(-pi/2),   0],
+                  [          0,     1,            0,  0],
+                  [-sin(-pi/2),     0,   cos(-pi/2),  0],
+                  [          0,     0,            0,  1]])
+
+    '''
+    R_y = Matrix([[ cos(-pi/2),     0,  sin(-pi/2)],
+                  [          0,     1,           0],
+                  [-sin(-pi/2),     0,  cos(-pi/2)]])
+
+    R_z = Matrix([[    cos(pi),     -sin(pi),   0],
+                  [    sin(pi),      cos(pi),   0],
+                  [          0,            0,   1]])
+'''
+    '''
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([test_case[0][1][0], test_case[0][1][1],
+                                                                   test_case[0][1][2], test_case[0][1][3]])
+    R_z_extrinsic = Matrix([[cos(yaw), -sin(yaw), 0],
+                            [sin(yaw), cos(yaw), 0],
+                            [0, 0, 1]])
+    R_y_extrinsic = Matrix([[cos(pitch), 0, sin(pitch)],
+                            [0, 1, 0],
+                            [-sin(pitch), 0, cos(pitch)]])
+    R_x_extrinsic = Matrix([[1, 0, 0],
+                            [0, cos(roll), -sin(roll)],
+                            [0, sin(roll), cos(roll)]])
+    XYZ_extrinsic_rot = eye(4)
+    XYZ_extrinsic_rot[0:3, 0:3] = R_z_extrinsic * R_y_extrinsic * R_x_extrinsic
+    '''
+    R_corr = R_z * R_y
+    T_total = T0_G * R_corr
+    print(Matrix(T_total.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0})))
 
     ## 
     ########################################################################################
@@ -82,10 +176,11 @@ def test_code(test_case):
 
     ## End your code input for forward kinematics here!
     ########################################################################################
-
+    ''' COMMENT OUT 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    pos_wc = T_total * np.array([0, 0, 0, 1])
+    your_wc = pos_wc[:,3][:3]  # <--- Load your calculated WC values in this array
+    your_ee = pos_wc[:,3][:3]  # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
@@ -131,7 +226,7 @@ def test_code(test_case):
         print ("End effector error for z position is: %04.8f" % ee_z_e)
         print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
 
-
+'''
 
 
 if __name__ == "__main__":
